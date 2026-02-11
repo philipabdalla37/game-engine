c7d8308b531b7a6ce520d394319653c6e01f363a
Ahmed Aly <aaly0632@gmail.com>
2026-02-11 18:42:20 -0500

    add api key

--- prompt_generator.py
+++ prompt_generator.py
@@ -6,6 +6,12 @@ import json
 class GPT_prompt_generator:
 
     output_prompts: list[str] = []
+    api_key= "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"
+    OpenAI = OpenAI()
+    client = OpenAI(
+    api_key= api_key
+    )
+
     def __init__(self, model = "gpt-5.2"):
         self.model = model
 
